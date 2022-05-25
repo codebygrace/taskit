a0c500058db907792fbab8c49ed1c6cdf3f3480d
Grace Mills <gracemills.1023@gmail.com>
2022-05-25 17:06:28 -0500

    set up use states for form text input

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+import React, {useState} from 'react';
 import './App.css';
 //importing components
 import Form from './components/Form';
 import ToDoList from './components/ToDoList';
 
 function App() {
+  const[inputText, setInputText] = useState("");
+  const[todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
         <h1>taskit - a to-do list app</h1>
       </header>
-      <Form/>
+      <Form todos={todos} setTodos={setTodos} setInputText={setInputText}/>
       <ToDoList/>
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,20 @@
 import React from "react";
 
-const Form = () => {
+const Form = ({setInputText, todos, setTodos}) => {
+  const inputTextHandler = (e) => {
+    console.log(e.target.value);
+    setInputText(e.target.value);
+  };
+  const submitTodoHandler = (e) => {
+    e.preventDefault();
+    setTodos([
+      ...todos, {text: inputText}
+    ])
+  }
   return (
     <form>
-      <input type="text" className="todo-input"></input>
-      <button className="todo-btn" type="submit">
+      <input onChange={inputTextHandler} type="text" className="todo-input"></input>
+      <button onClick={submitTodoHandler} className="todo-btn" type="submit">
         <i className="fa-solid fa-plus"></i>
       </button>
     </form>
