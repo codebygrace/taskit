82c0ad1c46f92fe47ae6e246e7cc51b1759ccabf
Grace Mills <gracemills.1023@gmail.com>
2022-06-02 18:54:26 -0500

    added responsive navbar functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, {useRef} from "react";
 
 function Navbar () {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive-nav");
+  }
+
   return (
     <div className="navbar">
-      <a href="../App.js">taskit</a>
-      <ul className="nav-list">
+      <a href="../App.js" className="logo">taskit</a>
+      <ul className="nav-list" ref={navRef}>
         <li className="nav-item">
           <a href="../App.js" className="nav-link">Home</a>
         </li>
         <li className="nav-item">
           <a href="../Contact.js" className="nav-link">Contact</a>
         </li>
+        <button onClick={showNavbar} className="close-btn">
+          <i class="fa-solid fa-xmark"></i>
+        </button>
       </ul>
+      <button onClick={showNavbar} className="hamburger-btn">
+        <i class="fa-solid fa-bars"></i>
+      </button>
     </div>
   )
 }
