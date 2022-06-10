df43c83e652695bb181e09b5f36c3638c6bb972a
Grace Mills <gracemills.1023@gmail.com>
2022-06-10 14:42:41 -0500

    updated case-sensitive file and folder names

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 
-import Header from './components/Header';
-import Form from './components/Form';
-import TodoList from './components/TodoList';
+import Header from './Components/Header';
+import Form from './Components/Form';
+import TodoList from './Components/TodoList';
 
 function App() {
   const[inputText, setInputText] = useState("");
